189b467c166352a0f799722415d1396757aad334
EdiedRamos <edieisa22@gmail.com>
2024-04-05 18:15:43 -0500

    chore: clear mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import { useEffect } from "react";
-
 import { Dashboard } from "@/Pages";
 
-import { useDashboardStore } from "./Store";
-
-import { DashboardMock } from "@/General/Data";
-
-import axios from "axios";
-
-const useInitialConfig = () => {
-  const { setBoards, setCurrentBoard } = useDashboardStore();
-
-  useEffect(() => {
-    setBoards(DashboardMock);
-    setCurrentBoard(DashboardMock.boardList[0].id);
-  }, []);
-
-  // * wake up backend :)
-  // useEffect(() => {
-  //   axios.get("https://social-network-fe9o.onrender.com");
-  //   const intervalId = setInterval(() => {
-  //     axios.get("https://social-network-fe9o.onrender.com");
-  //   }, 1000 * 60);
-  //   return () => clearInterval(intervalId);
-  // }, []);
-};
+const useInitialConfig = () => {};
 
 const App = () => {
   useInitialConfig();
